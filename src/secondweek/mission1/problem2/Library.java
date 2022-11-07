403bb176d1b83fb40572cb4d75d3bee66bf0836e
Luka <meena2003@naver.com>
2022-11-07 16:33:53 +0900

    <feat> Library 생성자 생성, 멤버변수 생성

---
+++ src/secondweek/mission1/problem2/Library.java
@@ -0,0 +1,11 @@
+package secondweek.mission1.problem2;
+
+public class Library {
+    private String title;
+    private String author;
+
+    public Library(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
