44bfcb4ad47efb0581f6afc270e6996a2a320151
Luka <meena2003@naver.com>
2022-11-07 17:06:28 +0900

    <feat> title, author getter 생성

--- src/secondweek/mission1/problem2/Book.java
+++ src/secondweek/mission1/problem2/Book.java
@@ -4,6 +4,14 @@ public class Book {
     private String title;
     private String author;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
