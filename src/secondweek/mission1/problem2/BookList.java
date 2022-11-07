179b25030cec94d029652b1a127f7c626880d5e8
Luka <meena2003@naver.com>
2022-11-07 17:17:08 +0900

    <refactor> 클래스명 BookList로 변경

---
+++ src/secondweek/mission1/problem2/BookList.java
@@ -0,0 +1,28 @@
+package secondweek.mission1.problem2;
+
+public class BookList {
+    private String name;
+    private Book[] bookshelf;
+    private Book[] alternativeBookshelf;
+
+    public BookList(String name) {
+        this.name = name;
+        bookshelf = new Book[5];
+    }
+
+    public void copyBooks() {
+        System.arraycopy(bookshelf, 0, alternativeBookshelf, 0, bookshelf.length);
+    }
+
+    public void addBook(Book newBook) {
+        int index =
+    }
+
+    public void printBooks() {
+        for (Book book : bookshelf) {
+            String title = book.getTitle();
+            String author = book.getAuthor();
+            System.out.printf("[%s|%s] ", title, author);
+        }
+    }
+}
