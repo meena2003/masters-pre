e4054f86865441c36dcbf7a8d0844d9fb320b992
Luka <meena2003@naver.com>
2022-11-07 17:39:11 +0900

    <feat> addBook, insertNewBook 메서드 생성

--- src/secondweek/mission1/problem2/BookList.java
+++ src/secondweek/mission1/problem2/BookList.java
@@ -2,24 +2,34 @@ package secondweek.mission1.problem2;
 
 public class BookList {
     private String name;
-    private Book[] bookshelf;
-    private Book[] alternativeBookshelf;
+    private Book[] bookList;
+    private int index;
 
     public BookList(String name) {
         this.name = name;
-        bookshelf = new Book[5];
-    }
-
-    public void copyBooks() {
-        System.arraycopy(bookshelf, 0, alternativeBookshelf, 0, bookshelf.length);
+        index = 0;
+        bookList = new Book[5];
     }
 
     public void addBook(Book newBook) {
-        int index =
+        if (index == bookList.length) {
+            bookList = extendsBookArray();
+        }
+        insertNewBook(newBook);
+    }
+
+    private void insertNewBook(Book newBook) {
+        bookList[index++] = newBook;
+    }
+
+    private Book[] extendsBookArray() {
+        Book[] tmpBookArray = new Book[bookList.length + 1];
+        System.arraycopy(bookList, 0, tmpBookArray, 0, bookList.length);
+        return tmpBookArray;
     }
 
     public void printBooks() {
-        for (Book book : bookshelf) {
+        for (Book book : bookList) {
             String title = book.getTitle();
             String author = book.getAuthor();
             System.out.printf("[%s|%s] ", title, author);
