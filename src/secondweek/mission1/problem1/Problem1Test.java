08380fdc02bc60c58d6cc88099cd1e3d6174b646
Luka <meena2003@naver.com>
2022-11-07 16:07:07 +0900

    알파벳 추가 메서드, 알파벳 출력 메서드 생성

---
+++ src/secondweek/mission1/problem1/Problem1Test.java
@@ -0,0 +1,22 @@
+package secondweek.mission1.problem1;
+
+public class Problem1Test {
+    static String[] alphabetArray = new String[27];
+    public static void main(String[] args) {
+        addAlphabets();
+        printAlphabets();
+    }
+
+    private static void addAlphabets() {
+        char alphabet = 65;
+        for (int i = 0; i < alphabetArray.length; i++) {
+            alphabetArray[i] = String.valueOf(alphabet++);
+        }
+    }
+
+    private static void printAlphabets() {
+        for (String alphabet : alphabetArray) {
+            System.out.print(alphabet);
+        }
+    }
+}
