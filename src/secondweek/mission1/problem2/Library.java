f6febe704ffbd6223e9307e5f3d0184314ac45fd
Luka <meena2003@naver.com>
2022-11-07 16:54:49 +0900

    <feat> Book 배열을 복사하는 copyBooks() 메서드 생성

--- src/secondweek/mission1/problem2/Library.java
+++ src/secondweek/mission1/problem2/Library.java
@@ -3,9 +3,16 @@ package secondweek.mission1.problem2;
 public class Library {
     private String name;
     private Book[] bookshelf;
+    private Book[] alternativeBookshelf;
 
     public Library(String name) {
         this.name = name;
         bookshelf = new Book[5];
     }
+
+    public void copyBooks() {
+        System.arraycopy(bookshelf, 0, alternativeBookshelf, 0, bookshelf.length);
+    }
+
+
 }
