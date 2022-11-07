0c095d878e4c1c97bf1334c103c2a87de3ebf8be
Luka <meena2003@naver.com>
2022-11-07 17:08:35 +0900

    <feat> printBooks 메서드 생성

--- src/secondweek/mission1/problem2/Library.java
+++ src/secondweek/mission1/problem2/Library.java
@@ -14,5 +14,11 @@ public class Library {
         System.arraycopy(bookshelf, 0, alternativeBookshelf, 0, bookshelf.length);
     }
 
-
+    public void printBooks() {
+        for (Book book : bookshelf) {
+            String title = book.getTitle();
+            String author = book.getAuthor();
+            System.out.printf("[%s|%s] ", title, author);
+        }
+    }
 }
