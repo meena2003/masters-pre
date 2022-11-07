3bfb8897c55ca16747e6ca7ea9ee144ad1b8c5c5
Luka <meena2003@naver.com>
2022-11-07 16:43:21 +0900

    Book 클래스 추가

---
+++ src/secondweek/mission1/problem2/Book.java
@@ -0,0 +1,11 @@
+package secondweek.mission1.problem2;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
