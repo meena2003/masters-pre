706d12261abd7bace305a4c644cb35b73ef4b26e
Luka <meena2003@naver.com>
2022-11-20 01:17:36 +0900

    A Very Big Sum 문제

--- src/thirdweek/algorithm/Problem4.java
+++ src/thirdweek/algorithm/Problem4.java
@@ -1,2 +1,40 @@
-package thirdweek.algorithm;public class Problem4 {
+package thirdweek.algorithm;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result4 {
+
+    public static long aVeryBigSum(List<Long> ar) {
+        long sum = 0;
+
+        for (int i = 0; i < ar.size(); i++) {
+            sum += ar.get(i);
+        }
+        return sum;
+    }
 }
+
+public class Problem4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result4.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
