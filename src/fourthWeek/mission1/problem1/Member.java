6b0a4b8367227511898657abf37240b4ad769fb9
Luka <meena2003@naver.com>
2022-11-21 15:41:01 +0900

    멤버십 프로그램 골격코드 복사

---
+++ src/fourthWeek/mission1/problem1/Member.java
@@ -0,0 +1,13 @@
+package fourthWeek.mission1.problem1;
+
+public abstract class Member {
+    abstract int applyDiscountRate(int shoppingAmount);
+
+    abstract int applyPointEarningRage(int shoppingAmount);
+
+    abstract int calculateParkingFee(int stayingHour);
+
+    void assignCounselor(String customerName) {
+        System.out.println(customerName + "님, 일반 상담원을 연결해드리겠습니다.");
+    }
+}
---
+++ src/fourthWeek/mission1/problem1/RED.java
@@ -0,0 +1,19 @@
+package fourthWeek.mission1.problem1;
+
+public class RED extends Member {
+
+    @Override
+    public int applyDiscountRate(int shoppingAmount) {
+        return shoppingAmount;
+    }
+
+    @Override
+    public int applyPointEarningRage(int shoppingAmount) {
+        return (int) (shoppingAmount * 0.01);
+    }
+
+    @Override
+    public int calculateParkingFee(int stayingHour) {
+        return stayingHour * 3000;
+    }
+}
