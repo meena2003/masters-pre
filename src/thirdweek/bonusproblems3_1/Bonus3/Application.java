b1dc4b56f2e38694c4091dd9747d5efc00f46b0e
Luka <meena2003@naver.com>
2022-12-01 18:17:58 +0900

    패키지명 수정

---
+++ src/thirdweek/bonusproblems3_1/Bonus3/Application.java
@@ -0,0 +1,18 @@
+package thirdweek.bonusproblems3_1.Bonus3;
+
+public class Application {
+    public static void main(String[] args) {
+        Point3D p = new Point3D(1, 2, 3);
+        System.out.println(p.toString() + "입니다.");
+
+        p.moveUp();
+        System.out.println(p.toString() + "입니다.");
+
+        p.moveDown();
+        p.move(10, 10);
+        System.out.println(p.toString() + "입니다.");
+
+        p.move(100, 200, 300);
+        System.out.println(p.toString() + "입니다.");
+    }
+}
---
+++ src/thirdweek/bonusproblems3_1/Bonus4/Application.java
@@ -0,0 +1,16 @@
+package thirdweek.bonusproblems3_1.Bonus4;
+
+public class Application {
+    public static void main(String[] args) {
+        PositivePoint p = new PositivePoint();
+
+        p.move(10, 10);
+        System.out.println(p.toString() + "입니다.");
+
+        p.move(-5, 5);
+        System.out.println(p.toString() + "입니다.");
+
+        PositivePoint p2 = new PositivePoint(-10, -10);
+        System.out.println(p2.toString() + "입니다.");
+    }
+}
